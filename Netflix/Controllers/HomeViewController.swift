c8ebd8a0ff89004f3065c416551fb04bc64f3d80
Gustavo Freitas <gustavo.rosa@madeinweb.app>
2020-08-14 21:03:52 -0300

    Inserted bar button items in the navigation controller, background image in the main film and general layout adjustments.

--- Netflix/Controllers/HomeViewController.swift
+++ Netflix/Controllers/HomeViewController.swift
@@ -21,35 +21,65 @@ class HomeViewController: UIViewController {
         
         setupLayout()
         navigationController()
+        tabBarViewController()
     }
+    
+    // MARK: - NavigationViewController
     
     func navigationController() {
         
-        let titleImageView = UIImageView()
-        titleImageView.image = UIImage(named: "logo")
-        titleImageView.contentMode = .scaleAspectFit
-        titleImageView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        let logoButton = UIButton(type: .system)
+//        logoButton.setImage(#imageLiteral(resourceName: "logo").withRenderingMode(.alwaysOriginal), for: .normal)
+        logoButton.setTitle("logo", for: .normal)
+        logoButton.setTitleColor(.white, for: .normal)
+        logoButton.contentMode = .scaleAspectFit
+        logoButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         
-        navigationItem.titleView = titleImageView
+        let myListButton = UIButton(type: .system)
+        myListButton.setTitle("My List", for: .normal)
+        myListButton.setTitleColor(.white, for: .normal)
+        myListButton.contentMode = .scaleAspectFit
+        myListButton.frame = CGRect(x: 0, y: 0, width: 130, height: 150)
+        
+        let moviesButton = UIButton(type: .system)
+        moviesButton.setTitle("Movies", for: .normal)
+        moviesButton.setTitleColor(.white, for: .normal)
+        moviesButton.contentMode = .scaleAspectFit
+        moviesButton.frame = CGRect(x: 0, y: 0, width: 130, height: 150)
+        
+        let tvShowsButton = UIButton(type: .system)
+        tvShowsButton.setTitle("Tv Shows", for: .normal)
+        tvShowsButton.setTitleColor(.white, for: .normal)
+        tvShowsButton.contentMode = .scaleAspectFit
+        tvShowsButton.frame = CGRect(x: 0, y: 0, width: 130, height: 150)
+        
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.backgroundColor = .clear
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.isTranslucent = false
-        navigationController?.navigationBar.backgroundColor = .black
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoButton)
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: myListButton), UIBarButtonItem(customView: moviesButton), UIBarButtonItem(customView: tvShowsButton)]
     }
+    
+    // MARK: - Title Screen
     
     let imageView: UIImageView = {
         
         let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "imageView")
+        imageView.image = #imageLiteral(resourceName: "irishman")
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
        return imageView
     }()
     
+    // MARK: - Sections
+    
     let previewSection: UILabel = {
         
         let preview = UILabel()
-        preview.text = "Previews"
+        preview.text = "  Previews"
         preview.textColor = .white
         preview.font = UIFont.boldSystemFont(ofSize: 20.0)
         preview.font = UIFont(name:"HelveticaNeue-Bold", size: 20.0)
@@ -57,10 +87,29 @@ class HomeViewController: UIViewController {
        return preview
     }()
     
+    // MARK: - MoviesDictionary
+    
     let data = [ CustomData(title: "movie_1", image: #imageLiteral(resourceName: "movie_1")),
                  CustomData(title: "movie_2", image: #imageLiteral(resourceName: "movie_2")),
-                 CustomData(title: "movie_3", image: #imageLiteral(resourceName: "movie_3"))
+                 CustomData(title: "movie_3", image: #imageLiteral(resourceName: "movie_3")),
+                 CustomData(title: "movie_4", image: #imageLiteral(resourceName: "movie_7")),
+                 CustomData(title: "movie_5", image: #imageLiteral(resourceName: "movie_18")),
+                 CustomData(title: "movie_6", image: #imageLiteral(resourceName: "movie_22")),
+                 CustomData(title: "movie_7", image: #imageLiteral(resourceName: "movie_20")),
+                 CustomData(title: "movie_8", image: #imageLiteral(resourceName: "movie_16")),
+                 CustomData(title: "movie_9", image: #imageLiteral(resourceName: "movie_15")),
+                 CustomData(title: "movie_10", image: #imageLiteral(resourceName: "movie_12")),
+                 CustomData(title: "movie_12", image: #imageLiteral(resourceName: "movie_19")),
+                 CustomData(title: "movie_13", image: #imageLiteral(resourceName: "movie_21")),
+                 CustomData(title: "movie_14", image: #imageLiteral(resourceName: "movie_8")),
+                 CustomData(title: "movie_16", image: #imageLiteral(resourceName: "movie_4")),
+                 CustomData(title: "movie_18", image: #imageLiteral(resourceName: "movie_14")),
+                 CustomData(title: "movie_19", image: #imageLiteral(resourceName: "movie_15")),
+                 CustomData(title: "movie_21", image: #imageLiteral(resourceName: "movie_18")),
+                 CustomData(title: "movie_23", image: #imageLiteral(resourceName: "movie_17")),
     ]
+    
+    // MARK: - UICollectionViewController
     
     let collectionView: UICollectionView = {
        
@@ -73,28 +122,63 @@ class HomeViewController: UIViewController {
         return collectionView
     }()
     
+    // MARK: - TabBarViewController
+    
+    func tabBarViewController() {
+        
+        let tabBar = UITabBarController()
+        tabBar.tabBar.barTintColor = .black
+        tabBar.tabBar.tintColor = .white
+        
+        let firstView = UIViewController()
+        let secondView = UIViewController()
+        let thirdView = UIViewController()
+        let fourthView = UIViewController()
+        let fifthView = UIViewController()
+        
+        let firstItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 0)
+        let secondItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
+        let thirdItem = UITabBarItem(tabBarSystemItem: .history, tag: 2)
+        let fourthItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 3)
+        let fifthItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 4)
+        
+        firstView.tabBarItem = firstItem
+        secondView.tabBarItem = secondItem
+        thirdView.tabBarItem = thirdItem
+        fourthView.tabBarItem = fourthItem
+        fifthView.tabBarItem = fifthItem
+        
+        tabBar.viewControllers = [firstView, secondView, thirdView, fourthView, fifthView]
+        
+        view.addSubview(tabBar.view)
+    }
+    
+    // MARK: - Custom Layout Controller
+    
     func setupLayout() {
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.backgroundColor = .black
         
         view.addSubview(imageView)
         view.addSubview(previewSection)
         view.addSubview(collectionView)
         
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: -30).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        NSLayoutConstraint.activate([
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: -30),
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         
-        previewSection.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
-        previewSection.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        previewSection.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        previewSection.topAnchor.constraint(equalTo: imageView.bottomAnchor),
+        previewSection.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        previewSection.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         
-        collectionView.backgroundColor = .red
-        collectionView.topAnchor.constraint(equalTo: previewSection.bottomAnchor, constant: 20).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+        collectionView.topAnchor.constraint(equalTo: previewSection.bottomAnchor, constant: 20),
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5)
+        ])
     }
 }
 
@@ -129,7 +213,7 @@ class MovieCell: UICollectionViewCell {
         let movieImage = UIImageView()
         movieImage.image = #imageLiteral(resourceName: "movie_2")
         movieImage.clipsToBounds = true
-        movieImage.layer.cornerRadius = 25
+        movieImage.layer.cornerRadius = 0.5
         movieImage.layer.masksToBounds = true
         movieImage.contentMode = .scaleAspectFill
         movieImage.translatesAutoresizingMaskIntoConstraints = false
